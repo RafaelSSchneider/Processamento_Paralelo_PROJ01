1ae525a557bdca43126f3b5724c78cfe622766cf
eberrsilva <eberrdasilva99@gmail.com>
2024-04-07 17:03:40 -0300

    Set a capacity for the clients queue. Added a validation in main method to not run it just once. Created a thread to run ClientFactory

--- src/main/java/project/BarberShop.java
+++ src/main/java/project/BarberShop.java
@@ -1,34 +1,39 @@
 package project;
 
+import static java.lang.Thread.startVirtualThread;
+
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BarberShop {
-	private static boolean hasStarted = false;
-	public static final AtomicBoolean POS_IN_USE = new AtomicBoolean(false);
 
-	public static AtomicReference<Queue> QUEUE = new AtomicReference<>(new PriorityQueue());
+	public static final AtomicBoolean POS_IN_USE = new AtomicBoolean(false);
+	private static final int CAPACITY = 20;
+	public static final AtomicReference<LinkedBlockingQueue<Client>> QUEUE = new AtomicReference<>(new LinkedBlockingQueue<>(CAPACITY));
+	private static boolean hasStarted = false;
 
 	public static void main(String[] args) {
+		if (hasStarted) {
+			return;
+		}
 		hasStarted = true;
 
-		var barber1 = new Barber("Artur").startThread();
-		var barber2 = new Barber("Éber").startThread();
-		var barber3 = new Barber("Rafael").startThread();
+		final var barber1 = new Barber("Artur");
+		final var barber2 = new Barber("Éber");
+		final var barber3 = new Barber("Rafael");
+
+		startVirtualThread(new ClientFactory());
+		final var threadBarber1 = startVirtualThread(barber1);
+		final var threadBarber2 = startVirtualThread(barber2);
+		final var threadBarber3 = startVirtualThread(barber3);
 
 		try {
-			barber1.join();
-			barber2.join();
-			barber3.join();
+			threadBarber1.join();
+			threadBarber2.join();
+			threadBarber3.join();
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
-
-		//create barbers
-		//crete pos
-		//create factory for consumers
-		//create queue for consumers
-		System.out.println("Hello world!");
 	}
 }
--- src/main/java/project/Client.java
+++ src/main/java/project/Client.java
@@ -14,6 +14,7 @@ public class Client implements Runnable {
 
 	public Client() {
 		this.name = randomName();
+		System.out.println(this.name + " created");
 	}
 
 	@Override
