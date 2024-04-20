0b053bbfaeb7b84a7c1c302f73e34f15dbd1a3fe
eberrsilva <eberrdasilva99@gmail.com>
2024-04-19 21:29:54 -0300

    Refatorada BarberShop para funcionar de forma mais parametrizada e criada listas para guardar os clientes que foram criados e clientes atendidos para saber se o programa está atendendo todos os usuarios corretamente

--- src/main/java/project/BarberShop.java
+++ src/main/java/project/BarberShop.java
@@ -2,38 +2,39 @@ package project;
 
 import static java.lang.Thread.startVirtualThread;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class BarberShop {
 
+	public static final int CAPACITY_COUCH = 4;
+	public static final int CAPACITY_QUEUE = 13;
 	public static final AtomicBoolean POS_IN_USE = new AtomicBoolean(false);
-	public static final Barber barber1 = new Barber("Artur");
-	public static final Barber barber2 = new Barber("Éber");
-	public static final Barber barber3 = new Barber("Rafael");
-	private static final int CAPACITY = 20;
-	public static final AtomicReference<LinkedBlockingQueue<Client>> QUEUE = new AtomicReference<>(new LinkedBlockingQueue<>(CAPACITY));
-	private static boolean hasStarted = false;
+	public static final List<Barber> BARBERS = List.of(new Barber("Artur"), new Barber("Éber"), new Barber("Rafael"));
+	public static final ClientQueue COUCH = new ClientQueue(CAPACITY_COUCH);
+	public static final ClientQueue QUEUE = new ClientQueue(CAPACITY_QUEUE);
+	public static final AtomicReference<List<String>> clientesCriados = new AtomicReference<>(new ArrayList<>());
+	public static final AtomicReference<List<String>> clientesAtendidos = new AtomicReference<>(new ArrayList<>());
 
 	public static void main(String[] args) {
-		if (hasStarted) {
-			return;
-		}
-		hasStarted = true;
-
 		startVirtualThread(new LoggerStatus());
 		startVirtualThread(new ClientFactory());
-		final var threadBarber1 = startVirtualThread(barber1);
-		final var threadBarber2 = startVirtualThread(barber2);
-		final var threadBarber3 = startVirtualThread(barber3);
 
-		try {
-			threadBarber1.join();
-			threadBarber2.join();
-			threadBarber3.join();
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
+		List<Thread> threadStream = BARBERS
+				.stream()
+				.map(Thread::startVirtualThread)
+				.toList();
+
+		threadStream.forEach(thread -> {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		});
+
+		System.out.printf("terminouuuuu");
 	}
 }
