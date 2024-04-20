7e8d7a00e3ad5102f34a08409e15fcd1d599be58
eberrsilva <eberrdasilva99@gmail.com>
2024-04-19 21:26:43 -0300

    Criada classe para especializar o comportamento do LinkedBlockingQueue, adicionando método para notificar clientes quando o podem ser atendidos

---
+++ src/main/java/project/ClientQueue.java
@@ -0,0 +1,26 @@
+package project;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.util.Objects.nonNull;
+import static project.LoggerStatus.log;
+import static project.Utils.sleep;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ClientQueue extends LinkedBlockingQueue<Client> {
+
+	public ClientQueue(int capacity) {
+		super(capacity);
+	}
+
+	public void notifyClient(Barber barber) {
+		log(this.getClass(), String.format("%s enviando notificacao", barber.getName()));
+		var client = this.poll();
+		if (nonNull(client)) {
+			barber.setClientInAttendance(client);
+			client.setBarber(barber);
+		}
+		sleep(300, MILLIS);
+		log(this.getClass(), String.format("%s finalizando notificacao", barber.getName()));
+	}
+}
