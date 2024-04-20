5755f7ff00578c1b1927313d9a0fc457a5a3c74b
eberrsilva <eberrdasilva99@gmail.com>
2024-04-19 21:30:53 -0300

    Refatorada LoggerStatus para controllar quais classes irão fazer o log, logs movidos para métodos para simplificar o código

--- src/main/java/project/LoggerStatus.java
+++ src/main/java/project/LoggerStatus.java
@@ -1,32 +1,97 @@
 package project;
 
-import static project.BarberShop.barber1;
-import static project.BarberShop.barber2;
-import static project.BarberShop.barber3;
-
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class LoggerStatus implements Runnable {
+
+	public static final Duration LOG_INTERVAL = Duration.of(2, ChronoUnit.SECONDS);
+	private static final Map<Class, Boolean> classActiveForLog = new HashMap<>() {{
+		put(Barber.class, true);
+		put(BarberShop.class, false);
+		put(Client.class, true);
+		put(ClientFactory.class, false);
+		put(ClientQueue.class, false);
+		put(Haircut.class, true);
+		put(LoggerStatus.class, true);
+		put(Utils.class, true);
+	}};
 
 	@Override
 	public void run() {
 		while (true) {
-			List<String> clientNames = BarberShop.QUEUE.get()
-					.stream()
-					.map(Client::getName)
-					.toList();
-
-			System.out.printf("\n\nClientes: " + clientNames);
-			System.out.printf("\nBarbeiro: %s -> %s", barber1.getName(), barber1.getClientInAttendance());
-			System.out.printf("\nBarbeiro: %s -> %s", barber2.getName(), barber2.getClientInAttendance());
-			System.out.printf("\nBarbeiro: %s -> %s", barber3.getName(), barber3.getClientInAttendance());
+			System.out.printf("\n");
+			logQueue();
+			logCouch();
+			logPOS();
+			logBarbers();
+			System.out.printf("\n");
+			logClientCriados();
+			logClientAtendidos();
 			System.out.printf("\n");
 
 			try {
-				Thread.sleep(500);
+				Thread.sleep(LOG_INTERVAL);
 			} catch (InterruptedException e) {
 				throw new RuntimeException(e);
 			}
 		}
+	}
+
+	public static void logQueue() {
+		List<String> clientQueue = BarberShop.QUEUE
+				.stream()
+				.map(Client::getName)
+				.toList();
+		log(LoggerStatus.class, String.format("LOG | Clientes na fila [%d]: %s", clientQueue.size(), clientQueue));
+	}
+
+	public static void logCouch() {
+		List<String> clientCouch = BarberShop.COUCH
+				.stream()
+				.map(Client::getName)
+				.toList();
+		log(LoggerStatus.class, String.format("LOG | Clientes no sofa [%d]: %s", clientCouch.size(), clientCouch));
+	}
+
+	public static void logClientAtendidos() {
+		List<String> clientCouch = BarberShop.clientesAtendidos.get()
+				.stream()
+				.toList();
+		log(LoggerStatus.class, String.format("LOG | Clientes atendidos [%d]: %s", clientCouch.size(), clientCouch));
+	}
+
+	public static void logClientCriados() {
+		List<String> clientCouch = BarberShop.clientesCriados.get()
+				.stream()
+				.toList();
+		log(LoggerStatus.class, String.format("LOG | Clientes criados [%d]: %s", clientCouch.size(), clientCouch));
+	}
+
+	public static void log(Class clazz, String string) {
+		var on = Optional.of(classActiveForLog.get(clazz))
+				.orElse(false);
+		if (on) {
+			String format = DateTimeFormatter.ISO_INSTANT.format(Instant.now());
+			System.out.printf("\n%s | %s", format, string);
+		}
+	}
+
+	private static void logBarbers() {
+		BarberShop.BARBERS
+				.forEach(barber -> log(LoggerStatus.class,
+						String.format("LOG | Barbeiro %s está %s", barber.getName(), barber.getClientInAttendance() == null
+								? "dormindo."
+								: "atendendo o cliente " + barber.getClientInAttendance().getName())));
+	}
+
+	public static void logPOS() {
+		log(LoggerStatus.class, "LOG | POS está em uso: " + (BarberShop.POS_IN_USE.get() ? "SIM" : "NAO"));
 	}
 }
